de871b2ff154f29553a73b404f2386c28db85fea
Guo Hong Wu <jorm7012@gmail.com>
2022-06-15 19:20:56 -0400

    Updated webpack & boilerplate
    Co-authored-by: Guo Hong Wu, Jenna Hamza, Joe Heinz, Jonah Hauanio, Garrett Layden

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
 
-// Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
-let root = document.createElement('div')
-
-root.id = 'root'
-document.body.appendChild(root)
+const container = document.getElementById('root');
+const root = createRoot(container);
 
 // Now we can render our application into it
-render(<App />, document.getElementById('root'))
+root.render(<App />);
