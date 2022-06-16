0bd311dfbcd2bfddee47229bed551df867655d0b
Joe Heinz <joeheinz99@gmail.com>
2022-06-15 20:27:28 -0400

    updates, css, webpack, redux boilerplate

--- src/index.js
+++ src/index.js
@@ -6,16 +6,10 @@ import { Provider } from 'react-redux';
 import store from './store';
 import '../assets/styles.css'
 
-
-// Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
-let root = document.createElement('div');
-root.id = 'root';
-document.body.appendChild(root);
-
 // Now we can render our application into it
-const content = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 
-content.render(
+root.render(
   <Provider store={store}>
     <App />
   </Provider>
